5188612c1beb3956e198944aa308f4b934e5cea8
simpl <justinpulley92@gmail.com>
2021-12-02 13:57:27 -0600

    RLE encoded UBER_NOUN.

--- packages/hardhat/scripts/uber_noun.js
+++ packages/hardhat/scripts/uber_noun.js
@@ -1,14 +1,14 @@
 const { PNGCollectionEncoder } = require("@nouns/sdk");
 const { readPngFile } = require("node-libpng");
 const fs = require("fs/promises");
-const { path } = require("path");
+const path = require("path");
 
 const DESTINATION = path.join(__dirname, "./output/image-data.json");
 
 const encode = async () => {
   const encoder = new PNGCollectionEncoder();
 
-  const folders = ["bodies", "accessories", "heads", "glasses"];
+  const folders = ["/bodies", "accessories", "heads", "glasses"];
   for (const folder of folders) {
     const folderpath = path.join(__dirname, "./images", folder);
     const files = await fs.readdir(folderpath);
