863c067be5358f3a39ef9f5fea9ef58810161f11
simpl <justinpulley92@gmail.com>
2021-12-02 18:50:17 -0600

    script generates SVG

--- packages/hardhat/scripts/uber_noun.js
+++ packages/hardhat/scripts/uber_noun.js
@@ -8,7 +8,7 @@ const DESTINATION = path.join(__dirname, "./output/image-data.json");
 const encode = async () => {
   const encoder = new PNGCollectionEncoder();
 
-  const folders = ["/bodies", "accessories", "heads", "glasses"];
+  const folders = ["bodies", "accessories", "heads", "glasses"];
   for (const folder of folders) {
     const folderpath = path.join(__dirname, "./images", folder);
     const files = await fs.readdir(folderpath);
