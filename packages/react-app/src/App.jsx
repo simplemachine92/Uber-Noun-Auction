0fca34812109a691b9f786161173d128dbc287d4
Justin Pulley <justinpulley92@gmail.com>
2021-12-08 01:48:39 -0600

    Remove burner display.

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -108,7 +108,7 @@ function App(props) {
   /* 🔥 This hook will get the price of Gas from ⛽️ EtherGasStation */
   const gasPrice = useGasPrice(targetNetwork, "fast");
   // Use your injected provider from 🦊 Metamask or if you don't have it then instantly generate a 🔥 burner wallet.
-  const userProviderAndSigner = useUserProviderAndSigner(injectedProvider, localProvider);
+  const userProviderAndSigner = useUserProviderAndSigner(injectedProvider);
   const userSigner = userProviderAndSigner.signer;
 
   useEffect(() => {
